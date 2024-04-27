4b9e1c437790472828e7fa67d6bed5fff209d65e
SebastianAvila <jesusaviala@gmail.com>
2024-04-27 09:52:02 -0600

    Correccion

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ const electronProcess = exec("electron .", { cwd: "../BotEscritorio" });
 
 // Ejecutar el proyecto de bot.peluqueria
 const botProcess = exec("npm run dev", { cwd: "./bot-peluqueria" });
-console.logh("Ok")
+console.log("Ok")
 // Manejar la salida de los procesos
 electronProcess.stdout.on("data", (data) => console.log(data));
 electronProcess.stderr.on("data", (data) => console.error(data));
